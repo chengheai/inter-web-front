9b8924d280c211b6dc3060a8b9380555c1218fd5
chengheai <842385139@qq.com>
2019-03-28 19:19:26 +0800

    add

---
+++ daguang/fun.1.js
@@ -0,0 +1,14 @@
+function foo() {
+	var sum = 0;
+	console.log(arguments);//伪数组或者类数组对象
+	//arguments[0]取到第一个实参值，arguments[arguments.length-1]取到最后一个实参值
+	//console.log(arguments[0],arguments[1])
+	//var sum = arguments[0]+arguments[1]+arguments[2]+arguments[3];
+
+	for (var i = 0; i < arguments.length; i++) {
+		sum += arguments[i]
+	}
+	document.write(sum);
+}
+
+foo(1, 2, 3, 4, 5, 55);
---
+++ daguang/fun.js
@@ -0,0 +1,15 @@
+function sum(){
+	var num = arguments[0];
+	if(arguments.length == 1){
+		return function(sec){
+			console.log(num+sec);
+		}
+	}else{
+		for(var i = 1; i < arguments.length; i++){
+			num += arguments[i];
+		}
+		console.log(num);
+	}
+}
+sum(2,3);            //5
+sum(2)(3);           //5
