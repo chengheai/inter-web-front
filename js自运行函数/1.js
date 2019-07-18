6f8305e5995e4c70630ee39027fd50e5ab01e805
chengheai <842385139@qq.com>
2019-07-18 14:27:57 +0800

    add IIFE

---
+++ js自运行函数/1.js
@@ -0,0 +1,4 @@
+var name = 'alex';
+(function(){
+  console.log(name);
+})()
---
+++ js自运行函数/2.js
@@ -0,0 +1,9 @@
+var name = 'Tom';
+(function() {
+  if (typeof name == 'undefined') {
+    var name = 'Jack';
+    console.log('Goodbye ' + name);
+  } else {
+    console.log('Hello ' + name);
+  }
+})();
