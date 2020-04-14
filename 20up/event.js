195d2c1b9884439fe9a6d1b47e71113d4b3b8144
alex <alex@yibianxue.com>
2020-04-14 13:04:54 +0800

    'node-======browers'

---
+++ 20up/event.js
@@ -0,0 +1,45 @@
+console.log('start')
+setTimeout(() => {
+  console.log('timer1')
+  Promise.resolve().then(function () {
+    console.log('promise1')
+  })
+}, 0)
+setTimeout(() => {
+  console.log('timer2')
+  Promise.resolve().then(function () {
+    console.log('promise2')
+  })
+}, 0)
+Promise.resolve().then(function () {
+  console.log('promise3')
+})
+console.log('end')
+
+
+/****
+ * 浏览器结果
+ *
+ * start
+    end
+    promise3
+    timer1
+    promise1
+    timer2
+    promise2
+ *
+ *
+ * */
+/****
+ * node结果
+ *
+ * start
+    end
+    promise3
+    timer1
+    timer2
+    promise1
+    promise2
+ *
+ *
+ * */
